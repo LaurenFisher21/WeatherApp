e18ee40fed0cafc03bd038ee41e20836487dd05c
Lauren Fisher <paigemaster001@gmail.com>
2022-12-02 08:36:56 +0200

    Initialize project

---
+++ JS/api-script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+import 'dotenv/config';
+import express from 'express'
+
+let weatherApi =
+{
+    /* fetching the data from the weather api */
+    fetchWeather: function (coord) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+            + coord +
+            "&units=metric&appid=" + process.env.WEATHER_API_KEY)
+            .then((res) => res.json())
+            .then((weatherdata) => this.displayWeather(weatherdata));
+    },
+    /* displaying the weather data */
+    displayWeather: function (weatherdata) {
+        const { name } = weatherdata;
+        const { description } = weatherdata.weather[0];
+        const { temp, humidity } = weatherdata.main;
+        const { speed } = weatherdata.wind;
+
+        $("#place").text("Weather in: " + name);
+        $("#temp").text(temp + " °C");
+        $("#humid").text(humidity);
+        $("#cloud").text(description);
+        $("#spped").text(speed + " km/h");
+        $(".weather-readings").removeClass("loading");
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-input").value);
+    }
+};
+
+/* search upon clicking search or hitting enter */
+$(".searchy").on("click", function () { weatherApi.search(); });
+$(".search-input").on("keyup", function (evt) { if (evt.key == "Enter") { weatherApi.search(); } });
+
+weatherApi.fetchWeather("Cape Town");
